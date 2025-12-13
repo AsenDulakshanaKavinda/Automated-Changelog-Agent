e525832a43315fa13820f196bb14e836fa54c7fb
Asen Dulakshana Kavinda <s22010362@ousl.lk>
2025-12-13 15:23:04 +0530

    and end poin to main

--- src/changelog_agent/webhook_service/app/main.py
+++ src/changelog_agent/webhook_service/app/main.py
@@ -1,12 +1,15 @@
 
 
+from fastapi import FastAPI
+from app.api.github_webhook import router as github_router
 
+# FastAPI app instance
+app = FastAPI(title='Webhook Service')
 
-
-
-
-
-
-
-
+# router inclusion
+app.include_router(
+    github_router,
+    prefix='/webhook/github',
+    tags=['Github Webhooks']
+)
 
